e16de360970cba1af00f34147a45a14dc91c9852
stefaniacamargo <estefaniacamargo263@gmail.com>
2022-07-22 10:29:48 -0500

    Cambios finales del proyecto.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import validator from './validator.js';
+import { maskify, validator } from "./validator.js";
 
-console.log(validator);
+const form = document.querySelector(".personal-data-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const creditCardNumber = formData.get("CreditCard");
+    if (validator(creditCardNumber)) {
+    alert("Your credit card is valid, thanks you for your purchase");
+    } else {
+    alert("Your credit card is invalid, please try again");
+    }
+});
+
+// Credit-Card
+
+
+const numberInput = document.querySelector("#CreditCard");
+const tcNumberDisplay = document.querySelector(".tc-number");
+
+numberInput.addEventListener("keyup", function (event) {
+    const creditCardNumber = event.currentTarget.value;
+    tcNumberDisplay.textContent = maskify(creditCardNumber);
+});
+
+const textInput = document.querySelector("#Name");
+const tcNameDisplay = document.querySelector(".tc-name");
+
+    textInput.addEventListener("keyup", function (event) {
+    const Name = event.currentTarget.value;
+    tcNameDisplay.textContent = (Name);
+});
+
+const dateInput = document.querySelector("#ExpirationDate");
+const tcDateDisplay = document.querySelector(".tc-date");
+
+    dateInput.addEventListener("keyup", function (event) {
+    const ExpirationDate = event.currentTarget.value;
+    tcDateDisplay.textContent = (ExpirationDate);
+    });
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
-const validator = {
-  // ...
-};
+/**
+ * @param {string} cardNumber
+ * @returns {boolean}
+ */
+  export function validator(cardNumber) {
+  const numberList = cardNumber.split("");
+  for (let index = 0; index < numberList.length; index += 2) {
+    const double = numberList[index] * 2;
+    if (double > 9) {
+      let sum = 0;
+      double
+        .toString()
+        .split("")
+        .forEach(function (number) {
+          sum += parseInt(number);
+        });
+      numberList[index] = sum;
+    } else {
+      numberList[index] = double;
+    }
+  }
 
-export default validator;
+  let totalSum = 0;
+  numberList.forEach(function (number) {
+    totalSum += parseInt(number);
+  });
+
+  if (totalSum % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ *
+ * @param {string} cardNumber
+ * @returns {string}
+ */
+export function maskify(cardNumber) {
+  if (cardNumber.length > 4) {
+    const numberList = cardNumber.split("");
+    for (let index = 0; index < numberList.length - 4; index++) {
+      numberList[index] = "#";
+    }
+    return numberList.join("");
+  } else {
+    return cardNumber;
+  }
+}
